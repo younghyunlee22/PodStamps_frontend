7b6ac7528e5f5e74d5db6f671fcebad575eb4248
Younghyun Lee <younghyunlee22@gmail.com>
2024-02-17 02:26:50 -0600

    delete folders

--- frontend/src/components/SpotifyPlayer.jsx
+++
@@ -1,8 +0,0 @@
-
-function SpotifyPlayer({link}) {
-  return (
-    <iframe style={{borderRadius: '12px'}} src={link} width="624" height="351" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-  );
-}
-
-export default SpotifyPlayer;
