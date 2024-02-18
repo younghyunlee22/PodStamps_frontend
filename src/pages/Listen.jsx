4d5365b65af74d910fe26c6e8667f40857b41029
Younghyun Lee <younghyunlee22@gmail.com>
2024-02-18 17:01:13 -0600

    Add a player button

--- src/pages/Listen.jsx
+++ src/pages/Listen.jsx
@@ -1,12 +1,23 @@
 import SpotifyPlayer from "../components/SpotifyPlayer.jsx";
+import {useState} from "react";
 
 const Listen = () => {
+
+    const [showPlayer, setShowPlayer] = useState(false);
+
+    function handleShowPlayer() {
+        console.log("add a player")
+            setShowPlayer(true);
+
+    }
     return (
         <div>
             <h1>Listen</h1>
             <h2>Spotify Player</h2>
-            <SpotifyPlayer
+            <button onClick={handleShowPlayer}>Add a player</button>
+            {showPlayer && <SpotifyPlayer
                 link={"https://open.spotify.com/embed/episode/59v1LmgmajUIAqXITdpz95/video?utm_source=generator&t=300"}/>
+            }
         </div>
     )
 }
