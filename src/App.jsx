01140f3d49b3b06554fcbf600229ee5f6ab6beca
Younghyun Lee <younghyunlee22@gmail.com>
2024-02-21 00:14:26 -0600

    sign in, search podcast shows

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ import Dashboard from "./pages/Dashboard.jsx";
 
 const code = new URLSearchParams(window.location.search).get("code");
 function App() {
-
+    console.log('code', code);
     return (
         <div style={{width:"100%"}}>
             <Header/>
             <Routes>
                 {/*page routing*/}
                 <Route path="/" element={<Home/>}/>
-                <Route path="/listen" element={<Listen/>}/>
+                <Route path="/listen" element={<Listen code={{code}}/>}/>
                 <Route path="/signin" element={<SignIn/>}/>
             </Routes>
             {code ? <Dashboard code={code}/> : <SignIn/>}
